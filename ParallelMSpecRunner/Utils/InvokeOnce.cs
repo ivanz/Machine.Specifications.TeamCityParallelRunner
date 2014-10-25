cbfc0f9dcd7cdf8d7c1ab61144661bb3a84d0478
Ivan Zlatev <i.zlatev@marketinvoice.com>
2014-10-25 22:26:32 +0100

    Make InvokeOnce thread-safe

--- ParallelMSpecRunner/Utils/InvokeOnce.cs
+++ ParallelMSpecRunner/Utils/InvokeOnce.cs
@@ -1,25 +1,30 @@
 using System;
+using System.Threading;
 
 namespace ParallelMSpecRunner.Utils
 {
     internal class InvokeOnce<T>
     {
         private readonly Action<T> _invocation;
+        private int _wasInvoked;
+
+        private const int TRUE = 1;
+        private const int FALSE = 0;
 
         public InvokeOnce(Action<T> invocation)
         {
             _invocation = invocation;
+            _wasInvoked = FALSE;
         }
 
         public void Invoke(T value)
         {
-            if (WasInvoked)
-                return;
-
-            WasInvoked = true;
-            _invocation(value);
+            if (Interlocked.CompareExchange(ref _wasInvoked, TRUE, FALSE) == FALSE)
+                _invocation(value);
         }
 
-        public bool WasInvoked { get; private set; }
+        public bool WasInvoked {
+            get { return _wasInvoked == TRUE; }
+        }
     }
 }
